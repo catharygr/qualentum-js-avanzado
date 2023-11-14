1778e7f69017b2ae38599698b91071a0502264e6
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-14 16:00:41 +0100

    Trabajando en la función fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ async function blogPost() {
   try {
     const response = await fetch(urls.blogPost);
     const data = await response.json();
+
+    const { posts } = data;
+
+    let html = "";
   } catch (error) {
     console.log(error);
   }
