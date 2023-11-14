55397a347a31e3ed94efbb32c087dd518e6da55f
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-14 16:09:49 +0100

    Iterando sobre toda la lista del blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,24 @@ async function blogPost() {
     const { posts } = data;
 
     let html = "";
+    posts.forEach((post) => {
+      const { id, title, content, date } = post;
+      html += `
+      <article class="post"><a class="post-link" data-id=${id}>
+					<header class="post-header">
+						<h3 class="post-title">${title}</h3>
+					</header>
+					<footer class="post-footer">
+						<p class="post-content">${content}{</p>
+						<p class="post-date">${date}</p>
+					</footer>
+				</a></article>
+      `;
+    });
   } catch (error) {
     console.log(error);
   }
+  blogListado.innerHTML = html;
 }
 
 blogPost();
