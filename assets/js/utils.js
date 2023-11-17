ae65f83fe63aebe863f9db719dbf45e566d58f5a
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-17 16:29:28 +0100

    Trabajando en la función para la fecha de publicación

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -24,14 +24,3 @@ export function getFechaPublicacion(fecha) {
     return `Hace ${segundos} segundos`;
   }
 }
-
-// Función que coge la fecha de publicación de un post y la retorna en formato es-ES
-export const formatDate = (date) => {
-  const options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  const dateFormatted = new Date(date).toLocaleDateString("es-ES", options);
-  return dateFormatted;
-};
